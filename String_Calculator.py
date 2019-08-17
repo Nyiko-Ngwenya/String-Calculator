5cb9d318a737d5a8810ac900c62c121fec7c356d
unknown <nyikongwenya@gmail.com>
2019-08-17 21:00:56 +0200

    First proper TDD

--- String_Calculator.py
+++ String_Calculator.py
@@ -1,42 +1,30 @@
-import regex as re
+import re
 import numpy as np
 
-def addition(a,b):
-    sum = 0
-    if a == '' or b == '':
-        if a == '' and b == '':
-            a = 0
-            b = 0 
-        elif b == '':
-            b == 0
-        else:
-            a = 0  
-    print(int(b))          
-    sum = int(a)+int(b)
-    return sum
+# def addition_proper(a):
+#     #arr = a.split(',')
+#     pattern = "\d+"
+#     #result = re.findall(pattern, string) 
+#     arrayy = re.findall(pattern, a) 
+#     print(arrayy)
+#     sum = 0
+#     for number in arrayy:
+#         #print(type(number))
+#         if number == '':
+#             number = 0
+#             #x = re.sub("\n", number)
+#         sum += int(number)
+#         print(sum)
+#     return sum
 
-def addition2(*args):
-    sum = 0
-    for number in args:
-        if number == '':
-            number = 0
-        sum += int(number)
-    return sum
-
-def addition_proper(a):
-    #arr = a.split(',')
-    pattern = "\d+"
-    #result = re.findall(pattern, string) 
-    arrayy = re.findall(pattern, a) 
-    print(arrayy)
-    sum = 0
-    for number in arrayy:
-        #print(type(number))
-        if number == '':
-            number = 0
-            #x = re.sub("\n", number)
-        sum += int(number)
-        print(sum)
-    return sum
-
+def Add(strings):
+    count = 0
+    if strings == '':
+        return 0
+    elif ',' in strings:
+        for number in strings.split(', | \n'):
+            count += int(number)
+        return count
+    else:
+        return int(strings)
 
--- test_String_Calculator.py
+++ test_String_Calculator.py
@@ -1,29 +1,27 @@
-from String_Calculator import addition,addition2,addition_proper
+from String_Calculator import Add
 
-def test_addition():
-    assert addition('','') == 0
-    assert addition('3','3') == 6
-    assert addition('','3') == 3
-    #assert addition('3', '3','3') == 9
-    #assert addition('3', '3','3','3') == 12
-    
-def test_addition2():
-    assert addition2('','') == 0
-    assert addition2('3','3') == 6
-    assert addition2('','3') == 3
-    assert addition2('3', '3','3') == 9
-    assert addition2('3', '3','3','3') == 12
-    assert addition2('3', '3','3','3') == 12
-    #assert addition2('1\n2,3') == 6
+def test_adding_empty_string():
+    result = 0
+    assert Add('') == result
 
-def test_addition_proper():
-    assert addition_proper('1,2') == 3
-    assert addition_proper('10,2,5,5') == 22
-    assert addition_proper('5,2') == 7
-    assert addition_proper('') == 0
-    assert addition_proper('1\n2,3') == 6
-    assert addition_proper('10\n5,5') == 20
-    assert addition_proper('//;\n1;2') == 3
-    assert addition_proper('//;\n1;-2') == -1
-
+def test_add_one_number():
+    result = 1
+    assert Add('1') == result
+    result = 10
+    assert Add('10') == result
             
+def test_add_two_numbers():
+    result = 3
+    assert Add('1,2') == result
+    result = 55
+    assert Add('50,5') == result
+
+def test_add_multiple_numbers():
+    result = 11
+    assert Add('1,2,8') == result
+    result = 60
+    assert Add('50,5,5') == result
+
+def test_add_with_new_line():
+    result = 6
+    assert Add('1\n2,3') == result
